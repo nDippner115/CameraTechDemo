9d0a1ba0828be068b5cfc3886877ed6295007c64
zgray37 <zacharygray14@augustana.edu>
2018-04-04 14:15:20 -0500

    Added Camera Activity code to the MainActivity class.

--- app/src/main/java/com/example/nicholasdippner14/cameratechdemo/MainActivity.java
+++ app/src/main/java/com/example/nicholasdippner14/cameratechdemo/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.nicholasdippner14.cameratechdemo;
 
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Camera mCamera = null;
+    private CameraView mCameraView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try{
+            mCamera = Camera.open();
+
+        }catch (Exception e){
+            Log.d("ERROR", "Failed to get camera: " + e.getMessage());
+        }
+
+       if(mCamera != null){
+            mCameraView = new CameraView(this,mCamera);
+           FrameLayout camera_view = (FrameLayout)findViewById(R.id.camera_view);
+           camera_view.addView(mCameraView);
+       }
+
+        ImageButton imgClose = (ImageButton)findViewById(R.id.imgClose);
+       imgClose.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               System.exit(0);
+           }
+       });
     }
 }
