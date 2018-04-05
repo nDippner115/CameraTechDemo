f441f929614ba15b87e255c3bc76583f88205c09
nicholasdippner14 <nicholasdippner14@augustana.edu>
2018-04-05 14:40:15 -0500

    Can take picture -preview fails after -pictures aren't saved?????

--- app/src/main/java/com/example/nicholasdippner14/cameratechdemo/MainActivity.java
+++ app/src/main/java/com/example/nicholasdippner14/cameratechdemo/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.nicholasdippner14.cameratechdemo;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.hardware.Camera;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -12,42 +15,61 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Camera mCamera = null;
     private CameraView mCameraView = null;
     private Button takePictureButton;
+    private ImageView mImageView = null;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    public void dispatchTakePictureIntent(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
+    }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mImageView = (ImageView) findViewById(R.id.imageView);
         takePictureButton = (Button) findViewById(R.id.button);
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE }, 0);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
         }
-        try{
+        try {
             mCamera = Camera.open();
 
-        }catch (Exception e){
+        } catch (Exception e) {
             Log.d("ERROR", "Failed to get camera: " + e.getMessage());
         }
 
-       if(mCamera != null){
-            mCameraView = new CameraView(this,mCamera);
-           FrameLayout camera_view = (FrameLayout)findViewById(R.id.camera_view);
-           camera_view.addView(mCameraView);
-       }
+        if (mCamera != null) {
+            mCameraView = new CameraView(this, mCamera);
+            FrameLayout camera_view = (FrameLayout) findViewById(R.id.camera_view);
+            camera_view.addView(mCameraView);
+        }
 
-        ImageButton imgClose = (ImageButton)findViewById(R.id.imgClose);
-       imgClose.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View view) {
-               System.exit(0);
-           }
-       });
+        ImageButton imgClose = (ImageButton) findViewById(R.id.imgClose);
+        imgClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.exit(0);
+            }
+        });
     }
 }
